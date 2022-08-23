36a309ad2ce450bd8d4c3dd8e108c725772e39b3
pierre0210 <conandoyle.0210@gmail.com>
2022-08-23 22:51:07 +0800

    initial commit

---
+++ src/bot.ts
@@ -0,0 +1,9 @@
+import { Client, Intents } from "discord.js";
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on("ready", () => {
+	console.log(`Logged in as ${client.user?.tag}`);
+});
+
+client.login(process.env.BOT_TOKEN);
